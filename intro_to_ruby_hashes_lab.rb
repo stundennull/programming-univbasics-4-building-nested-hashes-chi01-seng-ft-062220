1fe2316f27dc12dac84fd2fe680e0c436bd24537
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-16 07:46:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,17 +8,15 @@ end
 
 def monopoly_with_second_tier
   transportation_networks = {
-    :railroads => {:pieces => 4}
-    
+    :railroads => {:pieces => 4,
+    :rent_in_dollars => {},
+    :names => {} }
   }
 
 end
 
 def monopoly_with_third_tier
-  transportation_networks = {
-    :railroads => {:pieces => 4}, 
-    {:rent_in_dollars => {} },
-    {:names => {} }
+  
 end
 
 def monopoly_with_fourth_tier
