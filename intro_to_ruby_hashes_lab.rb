38820693e3a08f9d2b8049d5880aa7bfa921596b
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-16 07:38:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,8 @@ end
 def monopoly_with_third_tier
   transportation_networks = {
     :railroads => {:pieces => 4}, 
-    {:rent_in_dollars => {}
+    {:rent_in_dollars => {} },
+    {:names}
       
     }
 
